2043684a91029b6e6a4ada956b3dc67e3cef112b
anis <ldjzbillal@gmail.com>
2021-11-05 11:49:45 +0100

    ajouter une boucle pour trouver le nombre

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use std::io;
 use rand::{thread_rng,Rng};
+use std::cmp::Ordering;
 
 fn main () {
     
     let mut rng = thread_rng() ;
     let nombre_secret : u32 = rng.gen_range(1..101);
 
-    //saisie de l'utilisateur
-	let mut input : String = String::new();
-	println! ("devine mon nombre");
-	println! ("saisissez votre proposition");
-    io::stdin().read_line(&mut input).unwrap();
+    println! ("devine mon nombre");
+    println! ("saisissez votre proposition");
 
-    let input = input.trim().parse::<u32>().unwrap();  
-
-
-    //comparaison
-    if input == nombre_secret
+    loop
     {
-        println!("nombres egaux");
-    }
-    else
-    {
-        println!("nombres differents");
-    }
+        //saisie de l'utilisateur
+        let mut input : String = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim().parse::<u32>().unwrap();  
+
+
+        //comparaison
+        let result = input.cmp(& nombre_secret);
+        match result {
+            Ordering::Less => {
+                println!( "la proposition est trop petite");
+            }
+            Ordering::Equal => { 
+                println! (" c'est le bon nombre");
+                break;
+            }
+            Ordering::Greater => {
+                println!("la proposition est trop grande");
+            }
+        }
+    }   
 }
